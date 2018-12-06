32d60f499dc90f22857b1e61a99e3ccb1b08a006
Connor Penny <cgp19@duke.edu>
2018-12-06 16:33:37 -0500

    hopefully last time

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -121,7 +121,7 @@ public class HuffProcessor {
 		while(bit != -1) {
 			String code = codings[bit];
 			out.writeBits(code.length(), Integer.parseInt(code,2));
-			bit = in.readBits(BITS_PER_WORD+1);
+			bit = in.readBits(BITS_PER_WORD);
 		}
 		String last = codings[PSEUDO_EOF];
 		out.writeBits(last.length(), Integer.parseInt(last,2));
